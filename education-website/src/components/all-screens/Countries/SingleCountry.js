bad957c41b585daeab5a3c0a2ac71f28b9a071c1
matthewkromalic <jaykrommatt11@gmail.com>
2022-11-15 11:25:51 -0500

    Flags

--- education-website/src/components/all-screens/Countries/SingleCountry.js
+++ education-website/src/components/all-screens/Countries/SingleCountry.js
@@ -14,18 +14,8 @@ const SingleCountry = () => {
         setLoading(false)
       }
       fetchData();
-        // fetch(`https://restcountries.com/v2/name/${params.countryName}`)//.then((response) => console.log(response))
-        // .then((response) => response.json())
-        // .then((json) => setCountry(json))
-        // console.log(country)
-
     },[])
-        // if(country){
-        //   var loading = false
-        // }
-    // fetch(`https://restcountries.com/v2/name/${params.country.name}`)
-    // console.log(params)
-      // console.log(props.countryName)
+
       return(
         <div>
           {
@@ -33,6 +23,7 @@ const SingleCountry = () => {
           (<div>
             <h1>{country[0].name}</h1>
             <p>Capital : {country[0].capital}</p>
+            <img src={country[0].flags.svg}/>
           </div>)
           )
           }
