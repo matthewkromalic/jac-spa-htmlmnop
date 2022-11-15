f3bc979681407fda95d89474edd5ebe5f4604141
matthewkromalic <jaykrommatt11@gmail.com>
2022-11-15 14:47:51 -0500

    Far along API

--- education-website/src/components/all-screens/Countries/SingleCountry.js
+++ education-website/src/components/all-screens/Countries/SingleCountry.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 
 const SingleCountry = () => {
     const params = useParams();
@@ -22,11 +22,18 @@ const SingleCountry = () => {
           (loading ? <h3>Loading...</h3> :
           (<div>
             <h1>{country[0].name}</h1>
-            <p>Capital : {country[0].capital}</p>
+                                                                                           {/* take number and adds commas. found online */}
+            <p>{country[0].name} is a unique country with a capital of {country[0].capital} and a population of {country[0].population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}. 
+            One common language there is {country[0].languages[0].name} which is given the native name of {country[0].languages[0].nativeName}. The 
+            people from there are generally called {country[0].demonym}s. The currency in {country[0].name} is called {country[0].currencies[0].name} with 
+             a currency symbol of {country[0].currencies[0].symbol}. {country[0].name} is in {country[0].region} more specifically {country[0].subregion}</p>
             <img src={country[0].flags.svg}/>
           </div>)
           )
           }
+          <div>
+            <NavLink to = "/country">Back</NavLink>
+          </div>
         </div>
         )
 
