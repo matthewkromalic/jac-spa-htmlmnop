d4185c3a4b853fe1f0a60421f4f70f0e456dcb12
LarsenSam <sammuelarsen@gmail.com>
2022-11-14 22:35:51 -0500

    Made excellent progress on nasa page

--- education-website/src/components/all-screens/NASA/NasaAPI.js
+++ education-website/src/components/all-screens/NASA/NasaAPI.js
@@ -11,7 +11,7 @@ const NasaPage = () =>{
 
 
     useEffect(()=>{
-        fetch('https://images-api.nasa.gov/asset/as11-40-5874')
+        fetch('https://images-api.nasa.gov/search?q=apollo%2011')
         .then((response)=>response.json())
         .then((json)=>setImages(json));
 
@@ -25,8 +25,16 @@ const NasaPage = () =>{
             <h3>Learn about NASA</h3>
             {
                 (loading ? <h3>Loading...</h3>:
-                <img src ={images.collection.items[0].href} ></img>        
-              )
+                // console.log(images)
+                images.collection.items.map(item=> 
+                <div className="nasaElement" id={item.data[0].nasa_id}>
+                    
+                    <h3>{item.data[0].title}</h3>
+                    <p>{item.data[0].description}</p>
+                    {item.links ? <img src={item.links[0].href}></img>: <br></br>}
+                </div>)
+            
+                )
 
             }
         </div>
