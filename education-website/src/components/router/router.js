48f2753ae4ebedd3086f3766a0e49c744f88f55d
don90omar <113124856+don90omar@users.noreply.github.com>
2022-11-16 09:55:27 -0500

    updated contact page

--- education-website/src/components/router/router.js
+++ education-website/src/components/router/router.js
@@ -2,7 +2,7 @@ import { Route, Switch } from 'react-router-dom'
 import MainScreen from '../all-screens/main/MainScreen.js'
 import NasaPage from '../all-screens/NASA/NasaAPI.js'
 import Countries from '../all-screens/Countries/Country.js'
-import AboutOmar from '../all-screens/Contact/AboutPage.js'
+import AboutPage from '../all-screens/Contact/AboutPage.js'
 import SingleCountry from '../all-screens/Countries/SingleCountry.js'
 
 
@@ -13,7 +13,7 @@ const PageDirector = () => {
             <Route exact path={'/'} component={MainScreen}/>
             <Route exact path={'/country'} component={Countries}/>
             <Route exact path={'/nasa'} component={NasaPage}/>
-            <Route exact path={'/us'} component={AboutOmar}/>
+            <Route exact path={'/us'} component={AboutPage}/>
             {/* <Route exact path={'/singlecountry'} component={SingleCountry}/> */}
             {/* <Route exact path={'/singlecountry'} component={(props) => <SingleCountry {...props}/>}/> */}
             <Route exact path={'/singlecountry/:countryName'} component={SingleCountry}/>
