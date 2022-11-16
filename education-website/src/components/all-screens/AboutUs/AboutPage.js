0aec18fa6d680336b4a0fb75c97059db567c579a
don90omar <113124856+don90omar@users.noreply.github.com>
2022-11-16 14:10:54 -0500

    updated about page

--- education-website/src/components/all-screens/AboutUs/AboutPage.js
+++ education-website/src/components/all-screens/AboutUs/AboutPage.js
@@ -10,7 +10,7 @@ const AboutPage = () => {
         
         <div className="about">
              <h1>About Us</h1>
-             <h3>Omar Alkhulaidi</h3>
+             <h3>Omar </h3>
              <img src={Omar}></img>   
              <p>Hi! My name is Omar Alkhulaidi and I'm currently a student at WeCanCodeit. I have always admire and believe in technology and dreamed to become a Software Developer. I began my journey as a Software Developer with intensive full-stack java, back-end and front-end bootcamp and now dreams of landing my first job in the industry. I live in Hilliard, Ohio and participate in soccer every Sunday and Thursday</p>
              <a id="Linkedin" href="http://linkedin.com/in/omar-alkhulaidi-76ab111aa">linkedin</a>
