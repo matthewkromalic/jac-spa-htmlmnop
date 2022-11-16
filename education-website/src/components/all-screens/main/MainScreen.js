a5381a9c47b00a848df41f6be1d8c3e2ec09cb2f
matthewkromalic <jaykrommatt11@gmail.com>
2022-11-16 11:03:23 -0500

    random home

--- education-website/src/components/all-screens/main/MainScreen.js
+++ education-website/src/components/all-screens/main/MainScreen.js
@@ -2,21 +2,31 @@
 import { Link } from "react-router-dom";
 import "./main.css"
 import Question from "../images/question.gif"
-const MainScreen = () => (
+const MainScreen = () => {
 
-    
-    <div className="mainScreen">
-
+    function RNum() {
+            let test = Math.floor(Math.random() * 3);
+            if(test == 0){
+                return "/country"
+            }
+            else if(test == 1){
+                return "/nasa"
+            }
+            else{
+                return "/states"
+            }
+            } 
+    return(
+            <div className="mainScreen">
         <h1>Welcome to JAC'S Mystery Educator</h1>
+            <Link to={RNum()}>
+                <img src={Question}/>
+                </Link>   
 
-        <Link to="/country">
-         <img src={Question}/>
-         </Link>   
-
-         <p>Lets learn something new!</p>
-
-    </div>
-)
+            <p>Lets learn something new!</p>
+        </div>  
+    )
+}
 
 export default MainScreen
 
